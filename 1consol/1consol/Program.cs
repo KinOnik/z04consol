38c97e6ee46760a05cb5890bc3383f76f9be57b4
KinOnik <84587417+KinOnik@users.noreply.github.com>
2021-12-05 19:05:01 +0300

    Add files via upload

---
+++ 1consol/1consol/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1consol
+{
+    internal class Program
+    {
+        static double f(double n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n + f(n - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("\n\tВведите m:");
+                int m = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\n\tВведите k:");
+                int k = Convert.ToInt32(Console.ReadLine());
+                Console.Write($"\n\t Результат: {f(m) + f(2 * k)}");
+            }
+            catch (Exception)
+            {
+                Console.Write("\n\n\t****Ошибка ввода, некорректное число****\n\n");
+            }
+        }
+    }
+}
---
+++ 2consol/2consol/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _2consol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("\n\tВведите натуральное начальное число A: ");
+                int A = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\n\tВведите натуральное конечное число B: ");
+                int B = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\n\tРезультат:");
+                for (int i = A; i <= B; i++)
+                {
+                    Console.Write("\n\t");
+                    f(i);
+                }
+            }
+            catch(Exception)
+            {
+                Console.Write("\n\n\t****Введено некорректное число****\n\n");
+            }
+            
+        }
+
+        static void f(int n)
+        {
+            if (n!=0)
+            {
+                f(n / 10);
+            }
+            else return;
+            Console.Write(n % 10+" ");
+        }
+    }
+}
